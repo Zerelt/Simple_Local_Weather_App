395ab50a1bbcfe3f9e33e90e847e9e23164a17f4
Dobre Alexandru <axel.xander1@gmail.com>
2016-11-03 23:02:37 +0100

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+  var x = 0; //lat coord
+  var y = 0; //long coord
+  var showTempC; //display temp in Celsius
+  var showTempF; //display temp in Fahrenheit
+
+  if (navigator.geolocation) {
+
+    navigator.geolocation.watchPosition(
+      function(position) {
+        x = position.coords.latitude;
+        y = position.coords.longitude;
+        //position.coords.accuracy=10000; //any way to increase the accuracy ?
+        var accuracy = position.coords.accuracy;
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + x + "&lon=" + y + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json) {
+
+          /*note that the standard/default measuring unit for temperature that the openweather api uses is Kelvin; to get it to show the temperature in C or F you need to add &units=metric or &units=imperial after your api key; but i noticed that the temperatures that it returns like this were off by around 2 integers so i prefer manually converting them from kelvin;*/
+          showTempC = (json.main.temp - 273.15).toFixed(2);
+          showTempF = (json.main.temp * 9 / 5 - 459.67).toFixed(2);
+
+          $("#tempF").prop("checked", "checked");
+          $("#temp2").text(showTempF + " F");
+          $("#tempC").click(function() {
+            $("#temp2").text(showTempC + " ºC");
+          });
+          $("#tempF").click(function() {
+            $("#temp2").text(showTempF + " F");
+          });
+
+          $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+          $("#location").text(json.name + "," + json.sys.country);
+          $("#weather").text(json.weather[0].description);
+          $("#wind").text(json.wind.speed + " knots wind");
+        });
+
+        //If a city name is inputed:
+        if ($("#cityBtn").click(function() {
+            var city = $("#cityName").val();
+            $("#tempF").attr("checked", "checked");
+            if (city.length > 1) {
+              $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json2) {
+                showTempC = (json2.main.temp - 273.15).toFixed(2);
+                showTempF = (json2.main.temp * 9 / 5 - 459.67).toFixed(2);
+                $("#tempF").prop("checked", "checked");
+                $("#temp2").text(showTempF + " F");
+                $("#tempC").click(function() {
+                  $("#temp2").text(showTempC + " ºC");
+                });
+                $("#tempF").click(function() {
+                  $("#temp2").text(showTempF + " F");
+                });
+                $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + json2.weather[0].icon + ".png");
+                $("#location").text(json2.name + "," + json2.sys.country);
+                $("#weather").text(json2.weather[0].description);
+                $("#wind").text(json2.wind.speed + " knots wind");
+                x = json2.coord.lat;
+                y = json2.coord.lon;
+                //console.log(x +"," +y);
+
+                function initialize() {
+                  var mapProp = {
+                    center: new google.maps.LatLng(x, y),
+                    zoom: 12,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                  };
+                  var map = new google.maps.Map(document.getElementById("locationMap"), mapProp);
+                  var marker = new google.maps.Marker({
+                    position: mapProp.center,
+                    map: map,
+                    title: "You are here!"
+                  })
+
+                }
+                initialize();
+                //google.maps.event.addDomListener(document.getElementById("cityBtn"), 'click', initialize);
+
+              })
+
+            }
+          }))
+
+        //Map code:  
+        //by reading from http://www.w3schools.com/googleapi/google_maps_basic.asp;
+        //the marker by reading from: 
+        //http://www.w3schools.com/html/tryit.asp?filename=tryhtml5_geolocation_map_script 
+        //and 
+        //https://developers.google.com/maps/documentation/javascript/markers
+        function initialize() {
+          var mapProp = {
+            center: new google.maps.LatLng(x, y),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map(document.getElementById("locationMap"), mapProp);
+          var marker = new google.maps.Marker({
+            position: mapProp.center,
+            map: map,
+            title: "You are here!"
+          })
+
+        }
+          initialize();
+        //google.maps.event.addDomListener(window, 'load', initialize);
+
+      },
+      //the optional error function
+      function error() {
+        if (!navigator.geolocation) {
+          alert("You have denied access to your location");
+        }
+      },
+      //the optional PositionOptions object
+      {
+        maximumAge: 60000,
+        timeout: 50000,
+        enableHighAccuracy: true
+      }
+    )
+
+  }
+
+});
