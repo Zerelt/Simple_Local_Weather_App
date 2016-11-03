5166fe7c84d67d1bf7f88a9255bd3e026079b20a
Dobre Alexandru <axel.xander1@gmail.com>
2016-11-03 23:18:26 +0100

    changed getJSON request link

--- src/scripts.js
+++ src/scripts.js
@@ -14,9 +14,11 @@ $(document).ready(function() {
         //position.coords.accuracy=10000; //any way to increase the accuracy ?
         var accuracy = position.coords.accuracy;
 
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + x + "&lon=" + y + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json) {
+        $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + x + "&lon=" + y + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json) {
 
-          /*note that the standard/default measuring unit for temperature that the openweather api uses is Kelvin; to get it to show the temperature in C or F you need to add &units=metric or &units=imperial after your api key; but i noticed that the temperatures that it returns like this were off by around 2 integers so i prefer manually converting them from kelvin;*/
+          /*note that the standard/default measuring unit for temperature that the openweather api uses is Kelvin;
+          to get it to show the temperature in C or F you need to add &units=metric or &units=imperial after your api key;
+          but i noticed that the temperatures that it returns like this were off by around 2 integers so i prefer manually converting them from kelvin;*/
           showTempC = (json.main.temp - 273.15).toFixed(2);
           showTempF = (json.main.temp * 9 / 5 - 459.67).toFixed(2);
 
@@ -29,7 +31,7 @@ $(document).ready(function() {
             $("#temp2").text(showTempF + " F");
           });
 
-          $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+          $("#weatherIcon").attr("src", "https://crossorigin.me/http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
           $("#location").text(json.name + "," + json.sys.country);
           $("#weather").text(json.weather[0].description);
           $("#wind").text(json.wind.speed + " knots wind");
@@ -40,7 +42,7 @@ $(document).ready(function() {
             var city = $("#cityName").val();
             $("#tempF").attr("checked", "checked");
             if (city.length > 1) {
-              $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json2) {
+              $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=8dd6abbb435c2a67f45595e8e9ab4425", function(json2) {
                 showTempC = (json2.main.temp - 273.15).toFixed(2);
                 showTempF = (json2.main.temp * 9 / 5 - 459.67).toFixed(2);
                 $("#tempF").prop("checked", "checked");
@@ -51,7 +53,7 @@ $(document).ready(function() {
                 $("#tempF").click(function() {
                   $("#temp2").text(showTempF + " F");
                 });
-                $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + json2.weather[0].icon + ".png");
+                $("#weatherIcon").attr("src", "https://crossorigin.me/http://openweathermap.org/img/w/" + json2.weather[0].icon + ".png");
                 $("#location").text(json2.name + "," + json2.sys.country);
                 $("#weather").text(json2.weather[0].description);
                 $("#wind").text(json2.wind.speed + " knots wind");
@@ -81,11 +83,11 @@ $(document).ready(function() {
             }
           }))
 
-        //Map code:  
+        //Map code:
         //by reading from http://www.w3schools.com/googleapi/google_maps_basic.asp;
-        //the marker by reading from: 
-        //http://www.w3schools.com/html/tryit.asp?filename=tryhtml5_geolocation_map_script 
-        //and 
+        //the marker by reading from:
+        //http://www.w3schools.com/html/tryit.asp?filename=tryhtml5_geolocation_map_script
+        //and
         //https://developers.google.com/maps/documentation/javascript/markers
         function initialize() {
           var mapProp = {
